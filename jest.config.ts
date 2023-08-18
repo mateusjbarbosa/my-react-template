abf55f3a04a20c243f1bbdde9b9c55f62cb8823c
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-08-18 16:13:11 -0300

    style: linted files

--- jest.config.ts
+++ jest.config.ts
@@ -128,9 +128,7 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    '<rootDir>/.jest/setup.ts'
-  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -189,7 +187,7 @@ export default {
             legacyDecorator: true,
             decoratorMetadata: true,
             react: {
-              runtime: 'automatic'
+              runtime: 'automatic',
             },
           },
         },
